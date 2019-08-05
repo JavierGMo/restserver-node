7654db5029a8a979214dea48e5f84f34aef38819
JavGM <jinjuriki62@gmail.com>
2019-08-04 22:57:41 -0500

    RestServer-usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+}
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es oblifatoria']
+    },
+    //no es obligatoria
+    img: {
+        tipe: String,
+        required: false
+    },
+    //default: 'USER_ROLE'
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    //boolean
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    //boolean
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+usuarioSchema.plugin(uniqueValidator, {
+    message: '{PATH} debe de ser unico'
+});
+
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+
+}
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,39 +1,32 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.use(require('./routes/usuario.js'));
+
 // parse application/json
-app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario');
+
+
+//conexion a la base de datos
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de datos concetada');
+
 });
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
-
-    res.json({
-        persona: body
-    });
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
 
 app.listen(process.env.PORT, () => {
     console.log("Escucahndo en el puerto: ", process.env.PORT);
