96901f3ebacedeea46ab5890278156c6909c3de8
JavGM <jinjuriki62@gmail.com>
2019-08-04 23:05:53 -0500

    Solucion config

--- server/config/config.js
+++ server/config/config.js
@@ -22,10 +22,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//  urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://kriterFoock:lRwnrqq0VnZ37tkZ@cluster0-2srol.mongodb.net/cafe';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://kriterFoock:lRwnrqq0VnZ37tkZ@cluster0-2srol.mongodb.net/cafe';
+}
 
 process.env.URLDB = urlDB
