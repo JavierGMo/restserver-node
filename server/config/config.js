812d799917893ee5647f5e04126368183eec4136
JavGM <jinjuriki62@gmail.com>
2019-08-07 01:14:36 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -25,7 +25,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://kriterFoock:lRwnrqq0VnZ37tkZ@cluster0-2srol.mongodb.net/test';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB
